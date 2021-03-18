ad3b8786bdecce81c19e8ebfa7a41c8e6136b1aa
ttyhtg <zhm20081101@126.com>
2021-03-18 18:51:34 +0800

    hardway

---
+++ LearnPython3theHardWay/ex1.py
@@ -0,0 +1,7 @@
+print("Helle World")
+print("Helle Again")
+print("I like typing this")
+print("This is fun.")
+print("Yay!Printing.")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
