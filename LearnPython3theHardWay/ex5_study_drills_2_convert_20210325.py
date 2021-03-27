58a97dc873d9163820b9fa9c1d0bece9116f0a74
ttyhtg <zhm20081101@126.com>
2021-03-27 18:11:44 +0800

    update

---
+++ LearnPython3theHardWay/ex5_study_drills_2_convert_20210325.py
@@ -0,0 +1,9 @@
+# inches to centimeters
+i = 52
+c = i * 2.54
+print(f"{i}英寸等于{c}厘米")
+
+# pounds to kilograms
+p = 58
+k = p * 0.45359237
+print(f"{p}英镑等于{k:.2f}公斤")
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    """
+    request 就是作为参数传递进来的请求对象
+    :param request:
+    :return:
+    """
+    return HttpResponse("Hello world. You're at the polls index.")
