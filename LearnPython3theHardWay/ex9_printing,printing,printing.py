bc6486e745799f88531baf00eb487fdc1c96ac20
ttyhtg <zhm20081101@gmail.com>
2021-12-07 15:22:14 +0800

    hardway_ex9

---
+++ LearnPython3theHardWay/ex9_printing,printing,printing.py
@@ -0,0 +1,18 @@
+# Here is some new strange stuff, remember type it exactly
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days:", days)
+print("Here are the months:", months)
+
+print("""
+There's something going on here
+With the three double_quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
+
+# start the months on a new line
+months_new_line = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("Here are the months on a new line:", months_new_line)
